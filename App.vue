3b6ef2ddd83b53689cf67bdaa05f1f3c9b539567
itachi-zfy <83854796+itachi-zfy@users.noreply.github.com>
2024-08-16 08:04:01 +0800

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,45 +1,72 @@
-<script setup lang="ts">
-import * as Cesium from "cesium"
-import { onMounted } from 'vue'
+<script  lang="ts" setup>
+import HelloWorld from './components/HelloWorld.vue'
+import * as Cesium from 'cesium'
+import PositionProperty from 'cesium/Source/DataSources/PositionProperty';
+import { onMounted, version } from 'vue';
 onMounted(()=>{
   console.log(22222,Cesium);
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYjIwNTQyMi1kNDc0LTQ3ZDItYjZhYy1iNGI5OTgxMjM0NmMiLCJpZCI6MjI5NjAxLCJpYXQiOjE3MjE0ODMxMjl9.2T_Xgk-UDIHjDq_aT8Dk9OoLJIuXJqPKw9rWYpfp-_k';
+  
   const viewer = new Cesium.Viewer('cesiumContainer',{
-    timeline: false,
-    // imageryProvider: new Cesium.ArcGisMapServerImageryProvider({
-    //       url: "http://map.geoq.cn/ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer",
-    //       //url: "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer",
-    //     }),
-  //   terrainProvider: new Cesium.CesiumTerrainProvider({
-  //   url: 'https://assets.agi.com/stk-terrain/v1/tilesets/world/tiles', // 地形数据的URL
-  //   requestVertexNormals: true // 如果地形数据包含法线信息，可以设置为true
-  // }),
+    timeline:false,
+    terrainProvider: Cesium.createWorldTerrain({
+      requestWaterMask: true,
+      requestVertexNormals: true,
+    }),
 })
 
-const terrainProvider = new Cesium.CesiumTerrainProvider({
-  url:Cesium.IonResource.fromAssetId(3356),
-  requestWaterMask:true,//请求水体效果所需要的海岸线数据
-  requestVertexNormals:true,//请求地形照明数据
+var origin  = Cesium.Cartesian3.fromDegrees(114.399255, 30.539081, 10000);
+var modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(origin);
+var model = viewer.scene.primitives.add(Cesium.Model.fromGltf({
+  url:'http://192.168.3.65:8080/Apps/SampleData/models/CesiumAir/Cesium_Air.glb',
+  modelMatrix: modelMatrix,
+  scale:50
+}))
+viewer.camera.flyTo({
+  destination:Cesium.Cartesian3.fromDegrees(114.399255, 30.539081, 11000),
 })
-viewer.terrainProvider = terrainProvider 
-
+let czml = [
+  {
+    id:"document",
+    name:"CZML_Model",
+    version:"1.0"
+  },
+  {
+    id:"aircraft_model",
+    name:"Cesium Air",
+    position:{
+      cartographicDegrees:[114.399255, 30.539081, 10000]
+    },
+    model:{
+      gltf:"http://192.168.3.65:8080/Apps/SampleData/models/CesiumAir/Cesium_Air.glb",
+      scale:2.0
+    }
+  }
+]
+var dataSourcesPromise = viewer.dataSources.add(Cesium.CzmlDataSource.load(czml));
+dataSourcesPromise.then(function(dataSource){
+  viewer.trackedEntity = dataSource.entities.getById('aircraft_model');
 })
-
+  console.log(111111,Cesium);
+})
 </script>
 
 <template>
-  <div id = "cesiumContainer" class="cesium-container">
-    
+  <div id = "cesiumContainer" class="cesiumContainer">
+
   </div>
+
 </template>
 
+
 <style scoped>
-  .cesium-container {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: 0;
-    background-color: aqua;
-  }
+.cesiumContainer {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  margin: 0;
+  padding: 0;
+}
 </style>
